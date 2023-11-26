9d23104d1cc726cee4d5324bbf0403a9d938e53a
goldencorner <56497280+goldencorner@users.noreply.github.com>
2023-11-26 21:11:24 +0800

    Add files via upload

--- resin_and_signin.py
+++ resin_and_signin.py
@@ -1,5 +1,6 @@
 import pickle
 import subprocess
+import warnings
 
 from paddleocr import PaddleOCR, draw_ocr
 import datetime
@@ -29,9 +30,10 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 std_confidence = 0.9
+package_name = 'com.mihoyo.hyperion'
 
 
-def m_sleep(seconds):
+def get_soc():
     """获取电量"""
     # 使用 ADB 命令获取电池电量
     adb_command = "adb shell dumpsys battery | findstr \"level\""
@@ -49,15 +51,8 @@ def m_sleep(seconds):
     if battery_level is not None:
         print("电池电量：{}%".format(battery_level))
     else:
-        print("无法获取电池电量信息")
-
-    if battery_level > 50:
-        # 加大CPU负载
-        print(f"stress CPU {seconds}s")
-        os.system(f"adb shell \"(while true; do :; done) & sleep {seconds} && kill $!\"")
-    else:
-        print(f"sleep {seconds}s")
-        time.sleep(seconds)
+        raise "无法获取电池电量信息"
+    return battery_level
 
 
 def get_resolution():
@@ -141,11 +136,13 @@ class ResolutionScaleProblem(FloatProblem):
         image_resizer = ImageResizer(scale0, scale1, self.direction)
         resized_tuple = image_resizer.resize(template_, screenshot_)
         # 模板匹配
-        result = cv2.matchTemplate(resized_tuple[0], resized_tuple[1], cv2.TM_CCOEFF_NORMED)
-
-        # 找到匹配位置
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        solution.objectives[0] = -max_val
+        try:
+            result = cv2.matchTemplate(resized_tuple[0], resized_tuple[1], cv2.TM_CCOEFF_NORMED)
+            # 找到匹配位置
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            solution.objectives[0] = -max_val
+        except:
+            solution.objectives[0] = 0
 
         return solution
 
@@ -201,18 +198,25 @@ def solve_resolution_scale(template, screenshot, direction, max_seconds):
 
 def get_screenshot():
     os.system("adb shell screencap -p /sdcard/screen.png")
-    os.system("adb pull /sdcard/screen.png .")
+    datetime_now = datetime.datetime.now()
+    formatted_now = datetime_now.strftime('%d_%H_%M_%S')
+    screenshot_path = f"./screenshots/screen_{formatted_now}.png"
+    screenshot_path=f"./screenshots/screen.png"
+    os.system(f"adb pull /sdcard/screen.png {screenshot_path}")
+
+    # todo 直接读取到内存
+    return screenshot_path
 
 
 # 定义模板匹配函数
-def match_and_click(template_path):
+def match_and_click(template_path, save_coordinates=False):
     global std_confidence
     # 加载模板
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
     # 截图
-    get_screenshot()
+    screenshot_path = get_screenshot()
     # 加载截图
-    screenshot = cv2.imread("screen.png")
+    screenshot = cv2.imread(screenshot_path)
     # 转为灰度图像
     gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
     resolution = get_resolution()
@@ -272,6 +276,14 @@ def match_and_click(template_path):
     print("CONFIDENCE1", confidence)
     top_left = list(max_loc)
 
+    # debug 创建一个图形窗口并显示截图
+    # fig, ax = plt.subplots()
+    # ax.imshow(screenshot)
+    # ax.plot(max_loc[0], max_loc[1], 'ro')
+    # plt.savefig("test.jpg")
+    # plt.show()
+    # plt.close()
+
     '''第二次机会'''
     if max_val < std_confidence:
         print("默认参数失效，尝试更新参数")
@@ -310,45 +322,89 @@ def match_and_click(template_path):
     center = [(top_left[0] + bottom_right[0]) / 2, (top_left[1] + bottom_right[1]) / 2]
     center = image_resizer.restore_coordinates(center)
 
-    # debug 创建一个图形窗口并显示截图
-    # fig, ax = plt.subplots()
-    # ax.imshow(screenshot)
-    # ax.plot(center[0], center[1], 'ro')
-    # plt.savefig("test.jpg")
-    # plt.show()
-    # plt.close()
-
     if confidence > std_confidence:
+        # debug 创建一个图形窗口并显示截图
+        if save_coordinates:
+            fig, ax = plt.subplots()
+            ax.imshow(screenshot)
+            ax.plot(center[0], center[1], 'ro')
+            plt.savefig("test.jpg")
+            plt.show()
+            plt.close()
+
         # 点击模板中心位置
         os.system(
             "adb shell input tap {} {}".format(center[0], center[1]))
-        m_sleep(2)
+        time.sleep(2)
         return True
     else:
         print("NOT FOUND")
         return False
 
 
-def turn2resin_page():
+def turn2main_page():
     # 启动应用程序
-    package_name = 'com.mihoyo.hyperion'
     activity_name = '.main.HyperionMainActivity'
     subprocess.call(['adb', 'shell', 'am', 'start', '-n',
                      f'{package_name}/{package_name + activity_name}'])
-    m_sleep(2)
+    time.sleep(8)
+
+    # 处理主界面可能出现的弹窗
+    screenshot_path = get_screenshot()
+    result = get_OCR_result(screenshot_path)
+
+    for i in result:
+        if "青少年模式" in i[1][0]:
+            try:
+                print("try confirming teenager mode")
+                match_and_click("./templates/i_get_it.png")
+            except Exception as e:
+                warnings.warn("意料之外的识别错误: {}".format(str(e)))
+            time.sleep(3)
+            break
+        if "下次再说" in i[1][0]:
+            try:
+                print("try skipping update")
+                match_and_click("./templates/skip_update.png")
+            except Exception as e:
+                warnings.warn("意料之外的识别错误: {}".format(str(e)))
+            time.sleep(3)
+            break
+        if "米游社没有响应" in i[1][0]:
+            print("relaunch APP")
+            subprocess.call(['adb', 'shell', 'am', 'force-stop',
+                             f'{package_name}'])
+            time.sleep(8)
+            turn2main_page()
+            time.sleep(3)
+            break
+        if "回顶部" in i[1][0]:
+            center = i[0][0]
+            os.system(
+                "adb shell input tap {} {}".format(center[0], center[1]))
+            time.sleep(3)
+            break
+
+
+def turn2resin_page():
+    turn2main_page()
     match_and_click("./templates/myself.png")
-    match_and_click("./templates/my_roles.png")
-    m_sleep(20)
+
+    screenshot_path = get_screenshot()
+    result = get_OCR_result(screenshot_path)
+
+    for i in result:
+        if "成就达成数" in i[1][0]:
+            center = i[0][0]
+            os.system(
+                "adb shell input tap {} {}".format(center[0], center[1]))
+            break
+    time.sleep(20)
 
 
-def monitor_resin():
-    """从截图上识别体力值"""
-    # 截图
-    get_screenshot()
-    '''离线百度文本识别'''
+def get_OCR_result(screenshot_path):
     ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
-    img_path = 'screen.png'
-    result = ocr.ocr(img_path, cls=True)
+    result = ocr.ocr(screenshot_path, cls=True)
     result = result[0]
 
     # debug   显示结果
@@ -359,6 +415,16 @@ def monitor_resin():
     # im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
     # im_show = Image.fromarray(im_show)
     # im_show.save('result.jpg')
+
+    return result
+
+
+def monitor_resin():
+    """从截图上识别体力值"""
+    # 截图
+    screenshot_path = get_screenshot()
+
+    result = get_OCR_result(screenshot_path)
 
     result = str(result)
     result = result.replace(" ", "")
@@ -393,13 +459,9 @@ def monitor_resin():
 def sign_in():
     print("正在签到")
     # 启动应用程序
-    package_name = 'com.mihoyo.hyperion'
-    activity_name = '.main.HyperionMainActivity'
-    subprocess.call(['adb', 'shell', 'am', 'start', '-n',
-                     f'{package_name}/{package_name + activity_name}'])
-    m_sleep(2)
+    turn2main_page()
     match_and_click("./templates/sign_in.png")
-    m_sleep(8)
+    time.sleep(8)
     result = match_and_click("./templates/draw.png")
     return result
 
@@ -454,7 +516,28 @@ time_tolerance = 5 * 60 * 60
 os.system("adb devices")
 # 调用adb shell命令将亮度设置为0
 subprocess.run(["adb", "shell", "settings", "put", "system",
-                "screen_brightness", "50"])
+                "screen_brightness", "10"])
+
+
+def balance_SOC_or_sleep(seconds):
+    soc = get_soc()
+    if soc > 50:
+        # 增加CPU负载
+        print(f"stree cpu {seconds}s")
+        t0 = time.time()
+        turn2main_page()
+        while time.time() - t0 < seconds:
+            os.system("adb shell input swipe 500 1800 500 1000")
+            time.sleep(0.2)
+        time.sleep(3)
+
+        turn2main_page()
+
+    else:
+        print(f"sleep {seconds}s")
+        time.sleep(seconds)
+
+
 while True:
     # 检查今天是否已经签到
     # 加载上次签到的日期
@@ -464,47 +547,42 @@ while True:
     except FileNotFoundError:
         last_sign_in_day = None
     # 获取当前时间
-    now = datetime.datetime.now()
+    now = datetime.datetime.now() + datetime.timedelta(hours=-3)  # 米游社在零点会跳出一个弹窗 三点钟再签到避免这种情况
     # 如果当前时间是今天，并且上次签到不是今天，则执行签到
     if now.day != last_sign_in_day:
         result = sign_in()
         if result:
             last_sign_in_day = now.day
             try:
-                send_wechat("签到成功")
+                send_wechat(f"签到成功; soc:{get_soc()}")
             except:
                 pop_up_windows("签到成功")
             # 保存签到日期到磁盘上
             with open("last_sign_in_day.pkl", "wb") as f:
                 pickle.dump(last_sign_in_day, f)
 
-    turn2resin_page()
     start_time = time.time()
     while time.time() - start_time <= reset_threshold:
         try:
+            turn2resin_page()
             current_resin = monitor_resin()
             if current_resin >= 159:
                 try:
-                    send_wechat(f"current_resin:{current_resin}")
+                    send_wechat(f"current_resin:{current_resin}; soc:{get_soc()}")
                 except:
                     pop_up_windows("请求错误")
                 # 进入死循环，每10分钟查看一次，直到体力被消耗
                 while monitor_resin() >= current_resin:
+                    balance_SOC_or_sleep(reset_threshold)
                     turn2resin_page()
-                    m_sleep(reset_threshold)
+
             fault_num = 0
         except Exception as e:
-            try:
-                traceback.print_exc()
-                print("try confirming teenager mode")
-                match_and_click("./templates/i_get_it.png")
-                print("try skipping update")
-                match_and_click("./templates/skip_update.png")
-            except:
-                pass
+            traceback.print_exc()
+
             fault_num += 1
 
-        m_sleep(reset_threshold)
+        balance_SOC_or_sleep(reset_threshold)
 
     if fault_num * reset_threshold > time_tolerance:
         try:
